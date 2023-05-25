06bedc596a52d0a2c1ca0ff6adc2532f6cbb853c
Henrique Alves de Fernando <msft35@ribeiro.LABS-IC>
2023-05-25 20:19:07 -0300

    Implementação Obstacle

--- Aulas/Básico-C#/Códigos/JewelCollector/Obstacle.cs
+++ Aulas/Básico-C#/Códigos/JewelCollector/Obstacle.cs
@@ -2,4 +2,12 @@ namespace JewelCollector;
 
 public class Obstacle
 {
+    private string tipo {get; set;}
+    private Tuple<int, int> posicao {get; set;}
+
+    Obstacle(string tipo, Tuple<int, int> posicao){
+        this.tipo = tipo;
+        this.posicao = posicao;
+    }
+
 }
