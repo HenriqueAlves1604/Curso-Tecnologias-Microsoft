76586fb7939c7cf3506abcbf153b4e7e1934f6bd
Henrique Alves de Fernando <msft35@ribeiro.LABS-IC>
2023-05-25 22:34:14 -0300

    fixing more bugs

---
+++ Aulas/Básico-C#/Códigos/JewelCollector/AppMain.cs
@@ -0,0 +1,59 @@
+﻿namespace JewelCollector;
+public class AppMain {
+
+    public static void Main() {
+        bool running = true;
+        Map map = new Map();
+        Robot robot = new Robot(map);
+
+        //Adding jewels on the map:
+        map.addJewel("Red", 1, 9);
+        map.addJewel("Red", 8, 8);
+        map.addJewel("Green", 9, 1);
+        map.addJewel("Green", 7, 6);
+        map.addJewel("Blue", 3, 4);
+        map.addJewel("Blue", 2, 1);
+
+        //Adding obstacles on the map:
+        for(int i = 0; i < 7; i++){
+            map.addObstacle("Water", 5, i);
+        }
+        map.addObstacle("Tree", 5, 9);
+        map.addObstacle("Tree", 3, 9);
+        map.addObstacle("Tree", 8, 3);
+        map.addObstacle("Tree", 2, 5);
+        map.addObstacle("Tree", 1, 4);
+  
+    
+        do {
+            map.printMap();
+            Console.WriteLine("Enter the command: ");
+            char command = Console.ReadKey();
+            
+            switch(command){
+                case 'w':
+                    robot.moveUp();
+                    break;
+                case 'a':
+                    robot.moveLeft();
+                    break;
+                case 's':
+                    robot.moveDown();
+                    break;
+                case 'd':
+                    robot.moveRight();
+                    break;
+                case 'g':
+                    robot.collectJewel();
+                    break;
+                case 'q':
+                    running = false;
+                    break;
+            }
+            
+            robot.jewelsCollected();
+            Console.Write(" | ");
+            robot.totalPoints();
+        } while (running);
+    }
+}
